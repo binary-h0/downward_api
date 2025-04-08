a65f950834365de5f2f6b0df9a66ac1f98f8ef42
binary-h0 <max000807@gmail.com>
2025-04-08 16:16:34 +0900

    Impl: hw

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ pod_namespace = os.getenv("POD_NAMESPACE", "unknown-namespace")
 
 @app.route("/")
 def index():
-    return " Container EDU | POD Working : " + podname + " | nodename : " + node_name + " | namespace : " + pod_namespace + "\n"
+    return " Container EDU | POD Working : " + pod_name + " | nodename : " + node_name + " | namespace : " + pod_namespace + "\n"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
